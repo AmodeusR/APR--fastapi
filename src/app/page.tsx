879bde05bf5d308bca80d0026f4806af556160ca
AmodeusR <ricardogleiss@hotmail.com>
2024-05-07 22:28:06 -0300

    finish page design & backend implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,76 @@
 "use client";
 
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandItem,
+} from "@/components/ui/command";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [input, setInput] = useState("");
-  const [results, setResults] = useState<{ results: string[], duration: number }>();
+  const [searchResults, setSearchResults] = useState<{
+    results: string[];
+    duration: number;
+  }>();
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!input) return setResults(undefined);
+      if (!input) return setSearchResults(undefined);
 
-      await fetch(`/api/search?q=${input}`);
-    }
+      const response = await fetch(`https://apr--fastapi.amodeusr.workers.dev/api/search?q=${input}`);
+      const data = await response.json() as {results: string[], duration: number};
+      
+      setSearchResults(data);
+    };
 
-    fetchData();  
-  });
+    fetchData();
+  }, [input]);
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1>Speedsearch</h1>
-      <span>A high-performance API built with Hono, Next and Redis. Type a query below and get your response in miliseconds.</span>
-      <input className="bg-red-600" type="text" value={input} onChange={(e) => (setInput(e.target.value))} />
+    <main className="flex flex-col items-center h-screen grainy">
+      <div className="flex flex-col items-center mt-48 gap-6 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5 container">
+        <h1 className="font-bold text-5xl tracking-tight">SpeedSearch</h1>
+        <span className="text-zinc-600 text-lg max-w-prose text-center">
+          A high-performance API built with NextJS, Hono, Redis and Cloudfare.{" "}
+          <br /> Type a query below and get your response in miliseconds.
+        </span>
+
+        <div className="max-w-md w-full">
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={setInput}
+              placeholder="Search countries..."
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {searchResults?.results.length === 0 ? (
+                <CommandEmpty>No results found</CommandEmpty>
+              ) : null}
+
+              {searchResults?.results ? (
+                <CommandGroup heading="Results">
+                  {searchResults?.results.map((result) => (
+                    <CommandItem key={result} value={result} onSelect={setInput}>{result}</CommandItem>
+                  ))}
+                </CommandGroup>
+              ) : null}
+
+              {searchResults?.results ? (
+                <>
+                  <div className="h-px w-full bg-zinc-200"></div>
+                  <span className="p-2 text-xs text-zinc-500">
+                    Found {searchResults.results.length} in {searchResults.duration.toFixed(0)}ms
+                  </span>
+                </>
+              ): null}
+            </CommandList>
+          </Command>
+        </div>
+      </div>
     </main>
   );
 }
